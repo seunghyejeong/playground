ae74695108dfd8f9ffa9f76d7f4c7d8b94cb3f3b
seunghyejeong <hyhyhye90@gmail.com>
2023-05-07 18:21:27 +0900

    firstplay

---
+++ Basic230507.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var str = "Hello. First playgroud"
+
+str = "change"
+
+str
+
+
+var age = 20
+//age = "Hi"
+//-> Int 타입으로 이미 지정되어서 String 타입 넣을수 없음
+// error: error: Basic230507.playground:13:7: error: cannot assign value of type 'String' to type 'Int'
+
+
+// 큰숫자는 "_" 를 써서 읽기 쉽게 숫자를 표현할수 있음
+var money = 2_000_000
+var price = 1000200
